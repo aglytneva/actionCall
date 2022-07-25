682b0227ea2895a991e2f7861e9097cce364b005
Anastasiya Lytneva <kvartira-62@yandex.ru>
2022-07-26 01:05:30 +0300

    Initial commit

---
+++ app/src/main/java/com/example/activity/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.example.activity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val infoUser: TextView = findViewById(R.id.tvInfoToUser)
+        val buttonToStartCall: Button = findViewById(R.id.btnToCall)
+        val editNumber: EditText = findViewById(R.id.editNumber)
+
+        buttonToStartCall.setOnClickListener() {
+
+            val number = editNumber.getText().toString()
+             if (number.length == 10) {
+                val intent = Intent(this, SecondActivity::class.java)
+                Intent(intent)
+                intent.putExtra("number", number)
+                startActivity(intent);
+            }
+//        else infoUser.text = "Введите 10 цифр номера телефона"
+
+//        buttonToStartCall.setOnClickListener {
+//            val number = in.getText().toString()
+//            val intent = Intent(this, WeatherActivity::class.java)
+//            Intent(intent)
+//            intent.putExtra("city", city)
+//            startActivity(intent);
+//            val text = start.getText().toString()
+//            val intent = Intent()
+//            intent.putExtra("text", text)
+//            startActivity(Intent(this,SecondActivity::class.java))
+//
+
+        }
+
+
+    }
+
+
+}
+
+
+
---
+++ app/src/main/java/com/example/activity/SecondActivity.kt
@@ -0,0 +1,31 @@
+package com.example.activity
+
+import android.content.Intent
+import android.content.Intent.ACTION_CALL
+import android.net.Uri
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class SecondActivity: AppCompatActivity(R.layout.activity_second) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+        val number: TextView =findViewById<TextView>(R.id.tvNumberCall)
+
+
+        var numberFromFirstActivity :String = intent.getStringExtra("number").toString()
+        number.setText("Выполняем вызов на номер \n $numberFromFirstActivity")
+
+        val numberForCall ="+7"+numberFromFirstActivity
+
+
+//
+//        val intentNumber = Intent (ACTION_CALL)
+//        intentNumber.data = Uri.parse ("tel:" + numberForCall)
+//        startActivity (intentNumber)
+    }
+
+}
